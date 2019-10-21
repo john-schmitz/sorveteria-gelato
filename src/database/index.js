de98e90eb5f5b29e0668d55a6fbb010aa9e2f389
John <john.schmitz@madeinweb.com.br>
2019-10-21 19:55:48 -0300

    Move database

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
-const config = require('../../config/database.js');
+const config = require('./config/database.js.js');
 
-const Flavor = require('./Flavor');
+const Flavor = require('../models/Flavor');
 
 const connection = new Sequelize(config);
 
