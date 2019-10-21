c309c36a539012b14070ceb45aa0c02da81ddf84
John <john.schmitz@madeinweb.com.br>
2019-10-21 19:53:30 -0300

    Add database to tempalte

---
+++ src/models/Flavor.js
@@ -0,0 +1,20 @@
+const {
+  Model,
+  DataTypes
+} = require('sequelize');
+
+class Flavor extends Model {
+  static init(sequelize) {
+    super.init({
+      name: DataTypes.STRING,
+      calories_per_serving: DataTypes.DOUBLE,
+      product_line: DataTypes.STRING,
+      quantity: DataTypes.INTEGER,
+      price: DataTypes.DOUBLE
+    }, {
+      sequelize
+    })
+  }
+}
+
+module.exports = Flavor;
